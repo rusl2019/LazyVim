a36aa6597201fd9b2706f367bf646a748d6f6d34
rusl2019 <muhammad.ruslan14@gmail.com>
2024-04-23 19:56:05 +0700

    fix indent

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -55,7 +55,7 @@ opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
+opt.shiftwidth = 4 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
 opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
@@ -66,7 +66,7 @@ opt.spelllang = { "en" }
 opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
 opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
+opt.tabstop = 4 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 if not vim.g.vscode then
   opt.timeoutlen = 300 -- Lower than default (1000) to quickly trigger which-key
